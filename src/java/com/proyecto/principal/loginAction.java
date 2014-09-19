96a82d1bc9bd269685f05fef17488ab6c9a6c4cb
REY <REY@Rey-PC>
2014-09-18 21:43:50 -0500

    Agregue clase de login y validacion (falta cuando se conecte a la db)

---
+++ src/java/com/proyecto/principal/loginAction.java
@@ -0,0 +1,59 @@
+package com.proyecto.principal;
+
+import com.opensymphony.xwork2.*;
+public class loginAction extends ActionSupport{
+    private String user;
+    private String password;
+    
+    @Override
+    public String execute(){
+        if(user.equals("usuario")&& password.equals("12345")){
+            return SUCCESS;//"success"
+        
+        } else{
+            addFieldError("user","Usuario no valido");
+            addFieldError("password","Y/O password no valido");            
+            return INPUT;//"error" ERROR 
+        }
+    }
+    @Override
+    public void validate(){
+        if(user.equals("")){
+            addFieldError("user","El campo usuario no debe estar vacio");
+        }
+        if(password.equals("")){
+            addFieldError("password","Digite password por favor");
+        }
+        
+    }
+    
+
+    /**
+     * @return the user
+     */
+    public String getUser() {
+        return user;
+    }
+
+    /**
+     * @param user the user to set
+     */
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    /**
+     * @return the pwd
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the pwd to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
